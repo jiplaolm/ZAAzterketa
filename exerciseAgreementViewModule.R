1e370558aebbabacd69627e61e1e2e6e17a892da
Mikel Larrañaga <mikel.larranaga@ehu.eus>
2017-05-16 10:36:20 +0200

    Aldaketatxo bat

--- exerciseAgreementViewModule.R
+++ exerciseAgreementViewModule.R
@@ -48,9 +48,9 @@ exerciseAgreementViewModule <- function(input, output, session, data) {
     plot.data$Ariketa <- as.numeric(plot.data$Ariketa)
  
     ggplot(plot.data, aes(x=as.factor(Ariketa), y=Balioa, color=Irak, shape=Mota)) + 
-      geom_point( size=5) + #aes(shape=as.factor(Ev)),
+      #geom_point( size=5) + #aes(shape=as.factor(Ev)),
       xlab("Ariketa") + 
-      ylab("Balorazioa")  + geom_jitter()
+      ylab("Balorazioa")  + geom_jitter(width=.25, size=5, height = .25, alpha=.75)
   })
   
   # Erakutsi oharrak
